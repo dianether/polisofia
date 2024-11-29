ab66015e2619cd6d902cb9bb706b70be88b8bb55
dianether <dianemorales2002@gmail.com>
2024-11-29 17:32:42 -0600

    [feat] more components

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,23 +3,12 @@ import './index.css'
 import Footer from './Footer'
 import Header from './Header'
 import { createFB } from '../Firebase/api'
+import Card from '../Components/Card/Card'
 
 function Home() {
   return (
     <div className='home'>
         <Header/>
-        <div onClick={async() => { const guardar = await createFB(
-          {
-            id: 1,
-            user: "@juan",
-            text: "Este es un nuevo texto la primer publicación de prueba",
-            imgs: [
-              "https://picsum.photos/200",
-              "https://picsum.photos/200",
-              "https://picsum.photos/200"
-            ]
-          }, "publics"
-        ); console.log(guardar)}} style={{padding: "80px"}}>Crear</div>
         <Footer/>
     </div>
   )
